b8b43e7144ef17d067f20a4752845b3938a9aa68
younghyeong ryu <wangta69@naver.com>
2024-10-07 14:30:55 +0900

    t

--- src/database/migrations/2024_08_20_000002_create_social_account_table.php
+++ src/database/migrations/2024_08_20_000002_create_social_account_table.php
@@ -13,20 +13,22 @@ class  CreateSocialAccountTable extends Migration
    */
   public function up()
   {
-    Schema::create('social_accounts', function (Blueprint $table) {
-      $table->id();
-      $table->bigInteger('user_id')->unsigned();
-      $table->string('name', '50')->nullable();
-      $table->string('email', '50')->nullable();
-      $table->string('provider', '32');
-      $table->string('provider_id', '191');
-      $table->text('token')->nullable();
-      $table->text('refresh_token')->nullable();
-      $table->string('avatar', '191')->nullable();
-      $table->string('info')->nullable();
-      $table->timestamps();
-      $table->index(['provider', 'provider_id']);
-    });
+    if (!Schema::hasTable('social_accounts')) {
+      Schema::create('social_accounts', function (Blueprint $table) {
+        $table->id();
+        $table->bigInteger('user_id')->unsigned();
+        $table->string('name', '50')->nullable();
+        $table->string('email', '50')->nullable();
+        $table->string('provider', '32');
+        $table->string('provider_id', '191');
+        $table->text('token')->nullable();
+        $table->text('refresh_token')->nullable();
+        $table->string('avatar', '191')->nullable();
+        $table->string('info')->nullable();
+        $table->timestamps();
+        $table->index(['provider', 'provider_id']);
+      });
+    }
   }
 
   /**
