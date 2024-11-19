679d64bdb7ab84ebd3848e2c66813c4596e88916
younghyeong ryu <wangta69@naver.com>
2024-11-19 14:47:57 +0900

    Update 2024_08_20_000001_update_user_table.php
    t

--- src/database/migrations/2024_08_20_000001_update_user_table.php
+++ src/database/migrations/2024_08_20_000001_update_user_table.php
@@ -16,13 +16,66 @@ class  UpdateUserTable extends Migration
     if (Schema::hasTable('users')) {
       Schema::table('users', function($table)
       {
-        $table->string('mobile', '15')->nullable()->after('remember_token');
-        $table->integer('point')->nullable()->default(0)->after('remember_token');
-        $table->tinyInteger('active')->nullable()->default(0)->after('remember_token')->comment('1: active, 0: not active');
-        $table->timestamp('logined_at')->nullable()->after('updated_at');
-        $table->softDeletes()->after('updated_at');
+        if (!Schema::hasColumn('users', 'mobile')) {
+          Schema::table('users', function (Blueprint $table) {
+            $table->string('mobile', '15')->nullable()->after('remember_token');
+          });
+        } else {
+          // Schema::table('users', function (Blueprint $table) {
+          //   $table->string('mobile', '15')->nullable()->after('remember_token')->change();
+          // });
+        }
 
-        $table->string('password')->nullable()->change();
+        if (!Schema::hasColumn('users', 'point')) {
+          Schema::table('users', function (Blueprint $table) {
+            $table->integer('point')->nullable()->default(0)->after('remember_token');
+          });
+        } else {
+          // Schema::table('users', function (Blueprint $table) {
+          //   $table->integer('point')->nullable()->default(0)->after('remember_token')->change();
+          // });
+        }
+
+        if (!Schema::hasColumn('users', 'active')) {
+          Schema::table('users', function (Blueprint $table) {
+            $table->tinyInteger('active')->nullable()->default(0)->after('remember_token')->comment('1: active, 0: not active');
+          });
+        } else {
+          // Schema::table('users', function (Blueprint $table) {
+          //   $table->boolean('active')->nullable()->default(0)->after('remember_token')->comment('1: active, 0: not active')->change();
+          // });
+        }
+
+        if (!Schema::hasColumn('users', 'logined_at')) {
+          Schema::table('users', function (Blueprint $table) {
+            $table->timestamp('logined_at')->nullable()->after('updated_at');
+          });
+        } else {
+          // Schema::table('users', function (Blueprint $table) {
+          //   $table->timestamp('logined_at')->nullable()->after('updated_at')->change();
+          // });
+        }
+
+        if (!Schema::hasColumn('users', 'deleted_at')) {
+          Schema::table('users', function (Blueprint $table) {
+            $table->softDeletes()->after('updated_at');
+            // $table->timestamp('deleted_at')->after('updated_at');
+          });
+        } else {
+          // Schema::table('users', function (Blueprint $table) {
+          //   $table->softDeletes()->after('updated_at')->change();
+          // });
+        }
+
+        if (!Schema::hasColumn('users', 'password')) {
+          Schema::table('users', function (Blueprint $table) {
+            $table->string('password')->nullable();
+          });
+        } else {
+          Schema::table('users', function (Blueprint $table) {
+            $table->string('password')->nullable()->change();
+          });
+        }
       });
       // FULLTEXT INDEX `full` (`title`, `content`)
     }
@@ -40,7 +93,5 @@ class  UpdateUserTable extends Migration
       $table->string('password')->change();
       //password field nullable (for social login)
     });
-    
-      
   }
 }
