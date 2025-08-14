e14904deee01978f8beab4a0b50972166eaca525
pondol <y.h.ryu@tipszone.co.kr>
2025-08-14 10:00:06 +0900

    t
    t

--- src/Traits/AuthenticatedSession.php
+++ src/Traits/AuthenticatedSession.php
@@ -22,9 +22,7 @@ trait AuthenticatedSession {
     protected function _destroy($request)
     {
       Auth::guard('web')->logout();
-
       $request->session()->invalidate();
-
       $request->session()->regenerateToken();
     }
 
