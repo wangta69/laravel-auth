99e44521e86144d8e73e017e1664dfc5f1c96b01
pondol <y.h.ryu@tipszone.co.kr>
2026-05-29 17:03:57 +0900

    t
    t

--- src/Http/Controllers/PasswordResetLinkController.php
+++ src/Http/Controllers/PasswordResetLinkController.php
@@ -3,106 +3,104 @@
 namespace Pondol\Auth\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Password;
-use Illuminate\Auth\Events\PasswordReset;
-
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
-
+use Pondol\Auth\Events\ResetPasswordToken;
+// use Pondol\Auth\Notifications\sendEmailResetPasswordToken;
 use Pondol\Auth\Models\User\User;
 
-// use Pondol\Auth\Notifications\sendEmailResetPasswordToken;
-use Pondol\Auth\Events\ResetPasswordToken;
 // use App\Services\LocaleService;
 
 class PasswordResetLinkController extends Controller
 {
-  /*
-  |--------------------------------------------------------------------------
-  | Password Reset Controller
-  |--------------------------------------------------------------------------
-  |
-  | This controller is responsible for handling password reset emails and
-  | includes a trait which assists in sending these notifications from
-  | your application to your users. Feel free to explore this trait.
-  |
-  */
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller is responsible for handling password reset emails and
+    | includes a trait which assists in sending these notifications from
+    | your application to your users. Feel free to explore this trait.
+    |
+    */
 
-  use SendsPasswordResetEmails;
+    use SendsPasswordResetEmails;
 
-  public function __construct() // OpenSSLCryptoService $cryptoSvc
-  {
-  }
+    public function __construct() // OpenSSLCryptoService $cryptoSvc
+    {}
 
-  protected function create() {
-    // return view('pages/ko/auth/password_reset');
-    return view(auth_theme('user').'.forgot-password');
-  }
-
-  public function store(Request $request)
-  {
-    $validator = Validator::make($request->all(), [
-      'email' =>  ['required', 'email'],
-    ]);
-
-    // $validator =$request->validate([
-    //   'email' => ['required', 'email'],
-    // ]);
-
-    if ($validator->fails()) {
-      return redirect()->back()->withInput()->withErrors($validator->errors());
-      // return response()->json(['error'=>$validator->errors()->first()], 203);
+    public function create()
+    {
+        // return view('pages/ko/auth/password_reset');
+        return view(auth_theme('user').'.forgot-password');
     }
 
-      // We will send the password reset link to this user. Once we have attempted
-      // to send the link, we will examine the response then see the message we
-      // need to show to the user. Finally, we'll send out a proper response.
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => ['required', 'email'],
+        ]);
 
-    // $status = Password::sendResetLink(
-    // $status = $this->broker()->sendResetLink(
-    //   $request->only('email')
-    // );
-    $user = User::where('email', $request->email)->first();
-    if (!$user) {
-      return back()->withErrors(['email' => '등록되지 않은 이메일 입니다.']);
-    }
+        // $validator =$request->validate([
+        //   'email' => ['required', 'email'],
+        // ]);
 
-
-    // $user->notify(new sendEmailResetPasswordToken);
-    event(new ResetPasswordToken($user));
-    return back()->with(['status' => '등록된 이메일로 비밀번호 초기화 링크를 전송하였습니다.']);
-    // return $status === Password::RESET_LINK_SENT
-    // ? back()->with(['status' => __($status)])
-    // : back()->withErrors(['email' => __($status)]);
-
-  }
-
-  public function resetPassword(Request $request) {
-    $request->validate([
-      'token' => 'required',
-      'email' => 'required|email',
-      'password' => 'required|min:8|confirmed',
-    ]);
-
-    $status = Password::reset(
-      $request->only('email', 'password', 'password_confirmation', 'token'),
-      function (User $user, string $password) {
-        $user->forceFill([
-          'password' => Hash::make($password)
-        ])->setRememberToken(Str::random(60));
-
-          $user->save();
-
-          event(new PasswordReset($user));
+        if ($validator->fails()) {
+            return redirect()->back()->withInput()->withErrors($validator->errors());
+            // return response()->json(['error'=>$validator->errors()->first()], 203);
         }
-    );
 
-    return $status === Password::PASSWORD_RESET
-      ? redirect()->route('login')->with('status', __($status))
-      : back()->withErrors(['email' => [__($status)]]);
-  }
+        // We will send the password reset link to this user. Once we have attempted
+        // to send the link, we will examine the response then see the message we
+        // need to show to the user. Finally, we'll send out a proper response.
 
+        // $status = Password::sendResetLink(
+        // $status = $this->broker()->sendResetLink(
+        //   $request->only('email')
+        // );
+        $user = User::where('email', $request->email)->first();
+        if (! $user) {
+            return back()->withErrors(['email' => '등록되지 않은 이메일 입니다.']);
+        }
+
+        // $user->notify(new sendEmailResetPasswordToken);
+        event(new ResetPasswordToken($user));
+
+        return back()->with(['status' => '등록된 이메일로 비밀번호 초기화 링크를 전송하였습니다.']);
+        // return $status === Password::RESET_LINK_SENT
+        // ? back()->with(['status' => __($status)])
+        // : back()->withErrors(['email' => __($status)]);
+
+    }
+
+    public function resetPassword(Request $request)
+    {
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function (User $user, string $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password),
+                ])->setRememberToken(Str::random(60));
+
+                $user->save();
+
+                event(new PasswordReset($user));
+            }
+        );
+
+        return $status === Password::PASSWORD_RESET
+          ? redirect()->route('login')->with('status', __($status))
+          : back()->withErrors(['email' => [__($status)]]);
+    }
 }
--- src/Notifications/sendEmailResetPasswordToken.php
+++ src/Notifications/sendEmailResetPasswordToken.php
@@ -7,55 +7,50 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class sendEmailResetPasswordToken extends Notification  implements ShouldQueue
-
+class sendEmailResetPasswordToken extends Notification implements ShouldQueue
 {
-  use Queueable;
+    use Queueable;
 
-  /**
-   * Create a new notification instance.
-   *
-   * @return void
-   */
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct() {}
 
-  public function __construct()
-  {
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
 
-  }
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $token = app('auth.password.broker')->createToken($notifiable);
 
-  /**
-   * Get the notification's delivery channels.
-   *
-   * @param  mixed  $notifiable
-   * @return array
-   */
-  public function via($notifiable)
-  {
-    return ['mail'];
-  }
+        // 이메일을 쿼리 스트링으로 포함시킵니다.
+        $actionUrl = route('password.reset', [
+            'token' => $token,
+            'email' => $notifiable->getEmailForPasswordReset(),
+        ]);
 
-  /**
-   * Get the mail representation of the notification.
-   *
-   * @param  mixed  $notifiable
-   * @return \Illuminate\Notifications\Messages\MailMessage
-   */
-  public function toMail($notifiable)
-  {
-    $token=app('auth.password.broker')->createToken($notifiable);
-
-    // return (new MailMessage)
-    //   ->greeting('Hello!')
-    //   ->line('One of your invoices has been paid!')
-    //   ->action('View Invoice', $actionUrl)
-    //   ->line('Thank you for using our application!');
-    $actionUrl  = route('password.reset', [$token]);
-    return (new MailMessage)->subject('['.config('app.name').'] 비밀번호 초기화')->view(
-      auth_theme('mail').'.resetpassword',
-      [
-        'actionUrl' => $actionUrl,
-        'token' => $token,
-        'notifiable' => $notifiable
-      ]);
-  }
+        return (new MailMessage)
+            ->subject('['.config('app.name').'] 비밀번호 재설정 요청')
+            ->view(auth_theme('mail').'.resetpassword', [
+                'actionUrl' => $actionUrl,
+                'token' => $token,
+                'notifiable' => $notifiable,
+            ]);
+    }
 }
